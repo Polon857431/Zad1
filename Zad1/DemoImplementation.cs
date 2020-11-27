2f385f052d3412cafd5d7f7c54a0395f12788135
Karol <Karol@DESKTOP-TIIJ6VQ>
2020-11-27 06:11:56 +0100

    Dodaj pliki projektów.

---
+++ Zad1/DemoImplementation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DemoTarget;
+
+namespace DemoImplementation
+{
+
+    public static class DemoImplementation
+    {
+
+        public static IEnumerable<DemoTarget.PersonWithEmail> Flatten(IEnumerable<DemoSource.Person> people)
+        {
+            return from p in people
+                   from e in p.Emails
+                   select new PersonWithEmail
+                   {
+                       FormattedEmail = $"{e.Email} {e.EmailType}",
+                       SanitizedNameWithId = $"{p.Name} {p.Id}"
+                   };
+        }
+
+     
+
+    }
+
+}
+
